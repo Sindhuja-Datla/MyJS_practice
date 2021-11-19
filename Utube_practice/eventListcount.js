99b7ebf6e9cc96ea9986999c3989cb62109bb08c
Sindhuja-Datla <93333422+Sindhuja-Datla@users.noreply.github.com>
2021-11-19 22:42:40 +0530

    javascript

--- Utube_practice/eventListcount.js
+++
@@ -1,7 +0,0 @@
-function attachEvenListeners(){
-    let count=0;
-    document.getElementById("clickme").addEventListener("click",function xyx(){
-        console.log("button clicked",++count);
-    });
-}
-attachEvenListeners();
--- Utube_practice/sTimeOut.js
+++
@@ -1,8 +0,0 @@
-function x(){
-    for(let  i=1;i<=9;i++){
-        setTimeout(function(){
-            console.log(i);
-        },i*1000);
-    }
-}
-x();
